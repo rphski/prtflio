70a0ca81abdd5709e861622aedebbaafab04c114
Raphael <raphsamaral@gmail.com>
2023-12-28 22:01:12 -0300

    first commit

---
+++ _nuxt/static/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fcase\u002Fselected-projects","\u002Fabout","\u002Farchive","\u002F","\u002Fcase\u002Fgodspeed","\u002Fcase\u002Fflowers","\u002Fcase\u002Fshinjuku","\u002Fcase\u002Fspectral-caustics","\u002Fcase\u002Fmechanical","\u002Fcase\u002Fperplexiplex","\u002Fcase\u002Frnd"]})
